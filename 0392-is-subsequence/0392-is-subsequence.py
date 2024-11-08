9d38afc1bf865aef7462b183c8d89d631b714fc1
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2024-11-08 17:03:54 +0900

    Time: 3 ms (27.13%), Space: 16.6 MB (38.54%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,23 +1,38 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        # checking if s elements are in t
-        # for se in s:
-        #     if se not in t:
-        #         return False
-        # return True
+        # s = "abc"
+        # t = "ahbgdc"
 
-        # 순서도 고려해야함!
-        
-        s_idx = 0
-        t_idx = 0
-
-        while len(s) > s_idx and len(t) > t_idx :
+        s_idx = t_idx = 0
+        ans = []
+        while s_idx < len(s) and t_idx < len(t) :
             if s[s_idx] == t[t_idx]:
-                s_idx += 1  # s의 문자와 t의 문자가 같으면 s 인덱스 증가
-            t_idx += 1 # 항상 t 인덱스 증가
+                ans.append(s[s_idx])
+                s_idx += 1
+            t_idx += 1
+        
+        return len(ans) == len(s)
 
-    # return 키워드 뒤에 있는 표현식의 결과가 함수의 반환값으로 사용됩니다.
-    # 따라서 s_idx == len(s)가 True일 경우 함수는 True를 반환하고, False일 경우 함수는 False를 반환합니다.
-        return len(s) == s_idx
+
+
+    #     # 첫번째 풀이방법! (순서 고려 X)
+    #     # checking if s elements are in t
+    #     # for se in s:
+    #     #     if se not in t:
+    #     #         return False
+    #     # return True
+
+    #     # 순서도 고려해야함!
+    #     s_idx = 0
+    #     t_idx = 0
+
+    #     while len(s) > s_idx and len(t) > t_idx :
+    #         if s[s_idx] == t[t_idx]:
+    #             s_idx += 1  # s의 문자와 t의 문자가 같으면 s 인덱스 증가
+    #         t_idx += 1 # 항상 t 인덱스 증가
+
+    # # return 키워드 뒤에 있는 표현식의 결과가 함수의 반환값으로 사용됩니다.
+    # # 따라서 s_idx == len(s)가 True일 경우 함수는 True를 반환하고, False일 경우 함수는 False를 반환합니다.
+    #     return len(s) == s_idx
                     
         
