a15bec85e3f467dfb26ca9fad1f2953f044089d7
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2024-11-11 10:01:04 +0900

    Time: 0 ms (100%), Space: 18 MB (11.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,31 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        output = []
-        for i in range(len(nums)):
-            i_val = nums[i]
-            for j in range(1, len(nums)):
-                j_val = nums[j]
-                if i == j:
-                    break
-                if i_val + j_val == target :
-                    output.append(i)
-                    output.append(j)
-                    return output
+        info = {}
+        # info = {"2": 0}
+        for idx in range(len(nums)):
+            key = nums[idx]
+            # num1 + num2 = target
+            num1 = target - key
+            if num1 in info:
+                return([idx, info[num1]])
+            if key not in info: 
+                info[key] = idx
+                # return [num1, key]
+               
+        
+
+    
+
+
+        # output = []
+        
+        # for i in range(len(nums)):
+        #     i_val = nums[i]
+        #     for j in range(1, len(nums)):
+        #         j_val = nums[j]
+        #         if i == j:
+        #             break
+        #         if i_val + j_val == target :
+        #             output.append(i)
+        #             output.append(j)
+        #             return output
