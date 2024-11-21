64c83a34a0be5a6e3a70649f44810949ce6c8b60
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2024-11-21 14:12:43 +0900

    Time: 22 ms (51.78%), Space: 16.9 MB (21.69%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,27 +1,45 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        r_hashmap = {}
-        m_hashmap = {}
+        # r_hashmap = {}
+        # m_hashmap = {}
 
-        for r in ransomNote:
-            if r in r_hashmap:
-                r_hashmap[r] += 1
-            else:
-                r_hashmap[r] = 1
+        # for r in ransomNote:
+        #     if r in r_hashmap:
+        #         r_hashmap[r] += 1
+        #     else:
+        #         r_hashmap[r] = 1
 
-        for m in magazine:
-            if m in m_hashmap:
-                m_hashmap[m] += 1
-            else:
-                m_hashmap[m] = 1
+        # for m in magazine:
+        #     if m in m_hashmap:
+        #         m_hashmap[m] += 1
+        #     else:
+        #         m_hashmap[m] = 1
 
-        for r_key in r_hashmap:
-            if r_key not in m_hashmap:
-                return False
-            if r_hashmap[r_key] > m_hashmap[r_key]:
-                return False
+        # for r_key in r_hashmap:
+        #     if r_key not in m_hashmap:
+        #         return False
+        #     if r_hashmap[r_key] > m_hashmap[r_key]:
+        #         return False
         
+        # return True
+    
+
+    # magazine의 문자 빈도를 저장
+        char_count = {}
+
+        for char in magazine:
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+
+        # ransomNote를 순회하며 요구 문자 확인
+        for char in ransomNote:
+            if char not in char_count or char_count[char] == 0:
+                return False
+            char_count[char] -= 1
+
         return True
-                
+
 
         
