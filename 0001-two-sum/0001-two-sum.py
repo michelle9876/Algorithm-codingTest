6c5558a0959fee30f13ccf25d9fbd5b1df014304
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2024-12-06 12:04:15 +0900

    Time: 0 ms (100%), Space: 18.3 MB (12.62%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,31 +1,46 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        info = {}
-        # info = {"2": 0}
-        for idx in range(len(nums)):
-            key = nums[idx]
-            # num1 + num2 = target
+        # {key: number, value: num position}
+        dic = {}
+        for i in range(len(nums)):
+            value = i
+            key = nums[i]
+        # print(dic)
+        # num1 + key = target
             num1 = target - key
-            if num1 in info:
-                return([idx, info[num1]])
-            if key not in info: 
-                info[key] = idx
-                # return [num1, key]
+            if num1 in dic:
+                return [dic[num1], value]
+                
+            # add key,value in dic(cash) later
+            dic[key] = value
+        return [ ]
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+        # info = {}
+        # # info = {"2": 0}
+        # for idx in range(len(nums)):
+        #     key = nums[idx]
+        #     # num1 + num2 = target
+        #     num1 = target - key
+        #     if num1 in info:
+        #         return([idx, info[num1]])
+        #     if key not in info: 
+        #         info[key] = idx
+        #         # return [num1, key]
                
         
 
     
 
-
-        # output = []
-        
-        # for i in range(len(nums)):
-        #     i_val = nums[i]
-        #     for j in range(1, len(nums)):
-        #         j_val = nums[j]
-        #         if i == j:
-        #             break
-        #         if i_val + j_val == target :
-        #             output.append(i)
-        #             output.append(j)
-        #             return output
