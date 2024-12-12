fe51eb2b289c49354a752f9f2904ac5251e8a758
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2024-12-13 01:00:55 +0900

    Time: 8 ms (53.67%), Space: 18.2 MB (23.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,23 +2,21 @@ from collections import deque
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # uppercase --> lowercase
-        # remove all non-alphanumeric chars
+        # uppercase --> lowercase : lower()
+        # remove all non-alphanumeric chars : .isalnum()
         # read same forward and backward
         # for ch in s:
-        
         dq = deque()
 
         lowerCase = s.lower()
-        for lo in lowerCase:
-            if lo.isalnum() == True:
-                dq.append(lo)
-    
-
-        while len(dq) > 1 :
-            start = dq.popleft()
-            end = dq.pop()
-            if start != end:
+        for letter in lowerCase:
+            if letter.isalnum() == True:
+                dq.append(letter)
+        
+        while len(dq) > 1:
+            first = dq.popleft()
+            last = dq.pop()
+            
+            if first != last:
                 return False
-                 
         return True
