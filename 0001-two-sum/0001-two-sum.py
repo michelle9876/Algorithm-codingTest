fcc934c1c8a30d9cf43fd863e044383fc9f4121d
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2025-02-07 20:13:02 +0900

    Time: 0 ms (100%), Space: 18.7 MB (54.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,46 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # {key: number, value: num position}
+        # return  indices of the two numbers!
+        # dic = {2: 0, 7: 1, 11: 2, 15: 3} --> key: num, values: idx
+        # num1 + num2 = target ==> num1 = target - num2 
+
         dic = {}
         for i in range(len(nums)):
-            value = i
             key = nums[i]
-        # print(dic)
-        # num1 + key = target
-            num1 = target - key
-            if num1 in dic:
-                return [dic[num1], value]
-                
-            # add key,value in dic(cash) later
-            dic[key] = value
-        return [ ]
+            value = i
 
+            complement = target - key
+        
+            if complement in dic: # 이미 저장된 값인지 확인
+                return [i, dic[complement]]
 
-
+            dic[key] = i   # 현재 값과 인덱스를 저장
         
 
 
-
-
-
-
-
-
-
-
-        # info = {}
-        # # info = {"2": 0}
-        # for idx in range(len(nums)):
-        #     key = nums[idx]
-        #     # num1 + num2 = target
-        #     num1 = target - key
-        #     if num1 in info:
-        #         return([idx, info[num1]])
-        #     if key not in info: 
-        #         info[key] = idx
-        #         # return [num1, key]
-               
         
-
-    
-
