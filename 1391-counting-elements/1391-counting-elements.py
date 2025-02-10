2a7f968e89e640c64f9eccf1530d7365771a4300
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2025-02-10 19:54:06 +0900

    Time: 11 ms (8.82%), Space: 18.1 MB (12.11%) - LeetHub

--- 1391-counting-elements/1391-counting-elements.py
+++ 1391-counting-elements/1391-counting-elements.py
@@ -1,14 +1,12 @@
 class Solution:
     def countElements(self, arr: List[int]) -> int:
-        # Input: arr = [1,2,3]
-        # Output: 2
-        # Explanation: 1 and 2 are counted cause 2 and 3 are in arr.
-        
-        hashSet = set(arr)
         count = 0
-        for num in arr:
-            checkNum = num + 1
-            if checkNum in hashSet:
-                count += 1
         
+        for num in arr:
+            if num+1 in arr:
+                count += 1
         return count
+            
+            
+            
+        
