0a0d2e0fddf39962ea081df40e2b31a4a96f40c5
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2025-02-10 21:22:39 +0900

    Time: 3 ms (58.97%), Space: 18.1 MB (64.2%) - LeetHub

--- 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.py
+++ 2331-intersection-of-multiple-arrays/2331-intersection-of-multiple-arrays.py
@@ -3,15 +3,21 @@ class Solution:
     def intersection(self, nums: List[List[int]]) -> List[int]:
         count = defaultdict(int)
         n = len(nums)
-        dic = {}
-        for i in range(n) :
-            for j in range(len(nums[i])):
-                if nums[i][j] not in dic:
-                    count[nums[i][j]] += 1
-        # print(count)
+
+        for sublist in nums:
+            for num in set(sublist): # 각 리스트 내 중복 제거
+                count[num] += 1
         
-        # for key, value in count.items():
-        #     if value == n :
-        #         return [key]
-        key_result = [key for key, value in count.items() if value == n]
-        return sorted(key_result)
+        # 모든 리스트에서 등장한 숫자만 필터링하여 정렬된 리스트로 반환
+        return sorted([key for key, value in count.items() if value == n])
+        
+        # count = defaultdict(int)
+        # n = len(nums)
+        # dic = {}
+        # for i in range(n) :
+        #     for j in range(len(nums[i])):
+        #         if nums[i][j] not in dic:
+        #             count[nums[i][j]] += 1
+    
+        # key_result = [key for key, value in count.items() if value == n]
+        # return sorted(key_result)
