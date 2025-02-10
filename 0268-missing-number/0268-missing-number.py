6437acdf1eac41697ef874dcbc426d87cd485c60
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2025-02-10 19:13:07 +0900

    Time: 4 ms (49.96%), Space: 18.8 MB (49.16%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,11 +1,11 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-#         Input: nums = [0,1]
-#         Output: 2
-#         Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the           missing number 
+        n = len(nums)
+        nums_set = set(nums)
+       
+        for i in range(n+1):
+            if i not in nums_set:
+                return i
+            
+            
         
-        maxNum = len(nums)
-        # seen = set()
-        for num in range(maxNum + 1):
-            if num not in nums:
-                return num
