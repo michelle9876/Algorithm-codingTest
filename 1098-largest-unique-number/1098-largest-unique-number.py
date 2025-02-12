310186ea0383e2e33fe59dde19fc688421cbeefc
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2025-02-12 18:43:47 +0900

    Time: 0 ms (100%), Space: 17.8 MB (82.39%) - LeetHub

--- 1098-largest-unique-number/1098-largest-unique-number.py
+++ 1098-largest-unique-number/1098-largest-unique-number.py
@@ -7,5 +7,6 @@ class Solution:
             counts[num] += 1
             # print(counts)
          
-        once_nums = [key if value == 1 else -1 for key, value in counts.items()]
-        return max(once_nums) if once_nums else None
+        # once_nums = [key if value == 1 else -1 for key, value in counts.items()]
+        once_nums = [key for key, value in counts.items() if value == 1]
+        return max(once_nums) if once_nums else -1
