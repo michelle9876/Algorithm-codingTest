75bc4a27be340bbf536f123321492ed651051fda
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2025-02-17 17:32:30 +0900

    Time: 315 ms (68.77%), Space: 33.3 MB (82.15%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -4,12 +4,13 @@ class Solution:
         # Conditions1: a sum of digits equal to
         # return max sum
         # if no two numbers that satisfy , return -1
+        
         def get_digits_sum(num):
             # for num in nums:
             digit_sum = 0
             while num:
-                digit_sum += num % 10 
-                num //= 10 
+                digit_sum += num % 10  # 마지막 자리 숫자를 더함
+                num //= 10              # 마지막 자리 숫자 제거
             return digit_sum
         
         dic = defaultdict(list)
@@ -22,11 +23,11 @@ class Solution:
         for value in dic.values():
             # print(value)
             if len(value) > 1:
-                value.sort(reverse=True)
+                value.sort(reverse=True) # 내림차순 정렬
                 print(value)
-                max_sum = max(max_sum, value[0]+ value[1])
+                max_sum = max(max_sum, value[0]+ value[1]) # 가장 큰 두 개의 숫자 합 갱신
         return max_sum
-        
+
         # for key, value in dic.items():
         #     sum_num = 0
         #     if len(value) != 1: 
