d9f0505e73105eff68097074036e98b78c84fd79
BoGyeong Kim <50590112+michelle9876@users.noreply.github.com>
2025-05-02 18:29:29 +0900

    Time: 288 ms (19.18%), Space: 28.4 MB (9.74%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,49 +1,69 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
+        r = len(grid)
+        c = len(grid[0])
         seen = set()
         count = 0
+
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        def valid(row, col):
-            return 0 <= row < m and 0 <= col < n and grid[row][col] == '1'
+        def vaild(row, col):
+            return 0 <= row < r and 0 <= col < c and grid[row][col] == '1'
 
         def dfs(row, col):
-            for nr, nx in directions:
-                next_row, next_col = row + nr, col + nx
-                if valid(next_row, next_col) and (next_row, next_col) not in seen:
+            for dr, dc in directions:
+                next_row, next_col = row + dr, col + dc
+                if vaild(next_row, next_col) and (next_row, next_col) not in seen:
                     seen.add((next_row, next_col))
                     dfs(next_row, next_col)
-
-
-
-        for row in range(m):
-            for col in range(n):
+            
+        for row in range(r):
+            for col in range(c):
                 if grid[row][col] == '1' and (row, col) not in seen:
                     count += 1
                     seen.add((row, col))
                     dfs(row, col)
-
         return count
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         # m = len(grid)
         # n = len(grid[0])
+        # seen = set()
         # count = 0
+        # directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        # def valid(row, col):
+        #     return 0 <= row < m and 0 <= col < n and grid[row][col] == '1'
 
-        # def dfs(i, j):
-        #     if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != '1':
-        #         return
-        #     else:
-        #             grid[i][j] = '0'
-        #             dfs(i, j+1)
-        #             dfs(i+1, j)
-        #             dfs(i, j-1)
-        #             dfs(i-1, j)
+        # def dfs(row, col):
+        #     for nr, nx in directions:
+        #         next_row, next_col = row + nr, col + nx
+        #         if valid(next_row, next_col) and (next_row, next_col) not in seen:
+        #             seen.add((next_row, next_col))
+        #             dfs(next_row, next_col)
 
-        # for i in range(m):
-        #     for j in range(n):
-        #         if grid[i][j] == '1':  
+
+
+        # for row in range(m):
+        #     for col in range(n):
+        #         if grid[row][col] == '1' and (row, col) not in seen:
         #             count += 1
-        #             dfs(i, j)
+        #             seen.add((row, col))
+        #             dfs(row, col)
 
         # return count
+
+       
